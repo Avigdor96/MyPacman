e11ed863ec58aa7cf1855c0c4fc03a2acab66504
Avigdor96 <a0556691387@gmail.com>
2024-11-19 17:13:47 +0200

    Test

--- src/Main/Main.java
+++ src/Main/Main.java
@@ -4,6 +4,7 @@ import Graphic.GameFrame;
 
 public class Main {
     public static void main(String[] args) {
+        System.out.println("ll");
         GameFrame gameFrame = new GameFrame();
     }
 }
